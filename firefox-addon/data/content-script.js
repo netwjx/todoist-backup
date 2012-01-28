0fe87e28ad153c22f00b133dead80686dd5aaae0
netwjx <netwjx@gmail.com>
2012-01-28 23:22:43 +0800

    增加备份异步队列实现,以及简单的备份任务框架代码 已实现获取api token

--- firefox-addon/data/content-script.js
+++ firefox-addon/data/content-script.js
@@ -37,7 +37,82 @@ function assert(bool, msg){
     }).run();
 }());
 
+
+function Queue(ctx){
+    var tasks = Array.prototype.slice.call(arguments,1),
+        queue = {},
+        n = 0;
+    queue.done = function(){
+        var t = tasks.shift();
+        if(t && typeof t === 'function'){
+            console.debug('Queue task '+ (n++) + ' ' +
+                (t.toString().match(/function ([^\(]*)\(/)[1] || 'anonymous') + 
+                ' function is run;');
+
+            setTimeout(function(){
+                t.call(queue, ctx);
+            }, 0);
+        }
+        return queue;
+    };
+    return queue.done();
+}
 ///////////////////////////////////////////////////////////////////////////////
+
+// todoist 备份主过程
+function todoistBackup(success){
+    var ctx={};
+    Queue(ctx,
+        chooseBackupFile, 
+        receiveAPIToken, 
+        receiveProjects, 
+        receiveTodos, 
+        backupTodos,
+        success
+    );
+}
+
+// 选择备份文件路径
+function chooseBackupFile(ctx){
+    ctx.path = 'c:\\todoist_backup.json';
+    this.done();
+}
+
+// 获取备份时使用的Token
+function receiveAPIToken(ctx){
+    var self = this;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            var token = /var TOKEN = "([0-9a-f]{40})";/.exec(xhr.responseText)[1];
+            if(token){
+                ctx.token = token;
+                console.debug('receiveAPIToken ' + token);
+                self.done();
+                return;
+            }
+        }
+        // TODO error
+    };
+    xhr.open('GET', 'http://todoist.com/Users/viewPrefs?page=account', true);
+    xhr.send();
+}
+
+// 获取当前所有的project
+function receiveProjects(ctx){
+    this.done();
+}
+
+// 获取所有project下所有的todo,包括未完成和已完成的
+function receiveTodos(ctx){
+    this.done();
+}
+
+// 格式化并备份获取到的todo
+function backupTodos(ctx){
+    alert('备份完成');
+    this.done();
+}
 
 (function(){
     var link = document.createElement('a')
@@ -45,30 +120,10 @@ function assert(bool, msg){
     link.href = '#Backup';
     link.className = 'action go_premium';
     link.addEventListener('click', function(){
-        // TODO 考虑已任务队列的方式实现
-        selectBackupFile();
+        todoistBackup();
     });
 
     var child = document.getElementById('top_icons'),
         container = document.getElementById('top_right');
     container.insertBefore(link, child);
 }());
-
-
-function selectBackupFile(){
-    var cfg = {};
-    const nsIFilePicker = Components.interfaces.nsIFilePicker;
-    var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-    fp.init(window, '选择备份文件路径', nsIFilePicker.modeSave);
-    fp.appendFilters('*.json');
-    var rv = fp.show();
-    if(rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace){
-        cfg.file = fp.file;
-        cfg.path = fp.file.path;
-        RequestToken(cfg);
-    }
-}
-
-function RequestToken(cfg){
-    alert('ok');
-}
